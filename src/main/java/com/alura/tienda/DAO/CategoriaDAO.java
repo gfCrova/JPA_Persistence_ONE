2bca4b1d5ab43f8e6af74ac274082b98aa0f507a
gfCrova <giancrova.cl5@gmail.com>
2023-04-27 23:45:16 -0300

    add metodos actualizar, remover

--- src/main/java/com/alura/tienda/DAO/CategoriaDAO.java
+++ src/main/java/com/alura/tienda/DAO/CategoriaDAO.java
@@ -7,11 +7,18 @@ public class CategoriaDAO {
 
     private EntityManager em;
 
-    public CategoriaDAO(EntityManager em) {
-        this.em = em;
-    }
+    public CategoriaDAO(EntityManager em) { this.em = em; }
 
     public void guardar(Categoria categoria) {
         this.em.persist(categoria);
+    }
+
+    public void actualizar(Categoria categoria) {
+        this.em.merge(categoria);
+    }
+
+    public void remover(Categoria categoria) {
+        categoria = this.em.merge(categoria);
+        this.em.remove(categoria);
     }
 }

62d5fef3d31e1df563c3759228756c9e90a727d0
gfCrova <giancrova.cl5@gmail.com>
2023-04-28 23:04:55 -0300

    adding métodos consultarPorId() y consultarTodos()

--- src/main/java/com/alura/tienda/DAO/ProductoDAO.java
+++ src/main/java/com/alura/tienda/DAO/ProductoDAO.java
@@ -3,6 +3,7 @@ package com.alura.tienda.DAO;
 import com.alura.tienda.Entities.Producto;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class ProductoDAO{
 
@@ -14,5 +15,14 @@ public class ProductoDAO{
 
     public void guardar(Producto producto) {
         this.em.persist(producto);
+    }
+
+    public Producto consultarPorId(Long id) {
+        return em.find(Producto.class, id);
+    }
+
+    public List<Producto> consultarTodos() {
+        String jpql = "SELECT P FROM producto AS P";
+        return em.createQuery(jpql, Producto.class).getResultList();
     }
 }

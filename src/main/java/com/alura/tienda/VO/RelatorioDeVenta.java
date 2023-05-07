eb58fc5324fe7ea7fb863a9f9fc759eab38b3a05
gfCrova <giancrova.cl5@gmail.com>
2023-05-06 22:55:03 -0300

    package VO

--- src/main/java/com/alura/tienda/VO/RelatorioDeVenta.java
+++ src/main/java/com/alura/tienda/VO/RelatorioDeVenta.java
@@ -1,2 +1,49 @@
-package com.alura.tienda.VO;public class RelatorioDeVenta {
+package com.alura.tienda.VO;
+
+import java.time.LocalDate;
+
+public class RelatorioDeVenta {
+
+    private String nombreDelProducto;
+    private Long cantidadDeProducto;
+    private LocalDate fechaUltimaVenta;
+
+    public RelatorioDeVenta(String nombreDelProducto, Long cantidadDeProducto, LocalDate fechaUltimaVenta) {
+        this.nombreDelProducto = nombreDelProducto;
+        this.cantidadDeProducto = cantidadDeProducto;
+        this.fechaUltimaVenta = fechaUltimaVenta;
+    }
+
+    public String getNombreDelProducto() {
+        return nombreDelProducto;
+    }
+
+    public void setNombreDelProducto(String nombreDelProducto) {
+        this.nombreDelProducto = nombreDelProducto;
+    }
+
+    public Long getCantidadDeProducto() {
+        return cantidadDeProducto;
+    }
+
+    public void setCantidadDeProducto(Long cantidadDeProducto) {
+        this.cantidadDeProducto = cantidadDeProducto;
+    }
+
+    public LocalDate getFechaUltimaVenta() {
+        return fechaUltimaVenta;
+    }
+
+    public void setFechaUltimaVenta(LocalDate fechaUltimaVenta) {
+        this.fechaUltimaVenta = fechaUltimaVenta;
+    }
+
+    @Override
+    public String toString() {
+        return "RelatorioDeVenta { \n" +
+                "nombreDelProducto: '" + nombreDelProducto + '\'' + "\n" +
+                "cantidadDeProducto: " + cantidadDeProducto + "\n" +
+                "fechaUltimaVenta: " + fechaUltimaVenta + "\n" +
+                '}';
+    }
 }

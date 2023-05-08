a6b19c0851d9d469b00d3cc02d8d78e0b8dc5a66
gfCrova <giancrova.cl5@gmail.com>
2023-05-07 23:38:27 -0300

    clases heredadas de Producto

--- src/main/java/com/alura/tienda/Entities/Libros.java
+++ src/main/java/com/alura/tienda/Entities/Libros.java
@@ -1,2 +1,36 @@
-package com.alura.tienda.Entities;public class Libros {
+package com.alura.tienda.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "libros")
+public class Libros extends Producto{
+
+    private String autor;
+    private int paginas;
+
+    public Libros() {
+    }
+
+    public Libros(String autor, int paginas) {
+        this.autor = autor;
+        this.paginas = paginas;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
 }
